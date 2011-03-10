5018f90a75c11fd867bb03daf2e9829b330d8e38
Albert Bellonch <albert@itnig.net>
2011-03-10 18:33:47 +0100

    Refactoring markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
   
-  def get_markdown_html(text)
-      markdown = RDiscount.new(text)
-      markdown.to_html.html_safe
-  end
+  def get_markdown_html(text, options = {})
+     markdown = RDiscount.new(text)
+      html = markdown.to_html.html_safe
+      unless options[:max].nil?
+        html = truncate(html, :length => options[:max])
+      end
+      html
+   end
 end
