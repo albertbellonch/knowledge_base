7d6678acddd4a4bbdffccc50ba372f19dc75c2b7
Albert Bellonch <albert@itnig.net>
2011-03-10 10:23:57 +0100

    Modified route in order to set home/index controller as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 KnowledgeBase::Application.routes.draw do
+  resources :facts
+
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
