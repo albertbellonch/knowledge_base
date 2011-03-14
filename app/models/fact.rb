d2f2910000da6cc56dc61007ea0c56a902586cb2
Albert Bellonch <albert@itnig.net>
2011-03-14 14:28:22 +0100

    Added direct relation from fact to tags, apart from their indirect connection through the fact_tags table

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -3,6 +3,7 @@ class Fact < ActiveRecord::Base
   validates :description, :presence => true,
                     :length => { :minimum => 5 }
   has_many :fact_tags
+  has_many :tags, :through => :fact_tags
   has_many :comments
   belongs_to :user
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ActiveRecord::Base
   has_many :fact_tags
+  has_many :facts, :through => :fact_tags
 end
