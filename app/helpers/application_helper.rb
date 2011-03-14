d78fe24b0ba1eb2cd56bf3ae30804fa3e8536e26
Albert Bellonch <albert@itnig.net>
2011-03-14 12:55:16 +0100

    Now showing 'you' as author if a fact or comment is yours

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
   
   def get_markdown_html(text, options = {})
-     markdown = RDiscount.new(text)
-      html = markdown.to_html.html_safe
-      unless options[:max].nil?
-        html = truncate(html, :length => options[:max])
-      end
-      html
+    markdown = RDiscount.new(text)
+    html = markdown.to_html.html_safe
+    unless options[:max].nil?
+      html = truncate(html, :length => options[:max])
+    end
+    html
    end
+
+  def get_name_or_you(user)
+    (user == current_user.name)?"you":user
+  end
 end
