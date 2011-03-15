f654ac45a23b42ee60c810dcb483582a4d3d891d
Albert Bellonch <albert@itnig.net>
2011-03-15 10:54:12 +0100

    Fact's description length correction

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(:version => 20110311132035) do
 
   create_table "facts", :force => true do |t|
     t.string   "title"
-    t.text     "description", :limit => 255
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
