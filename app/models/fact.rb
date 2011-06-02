1e982d0dcd9e54c896a7ec3e454db0502996c410
Albert Bellonch <albert@itnig.net>
2011-06-02 17:41:39 +0200

    Validating that a fact has a user

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -2,6 +2,8 @@ class Fact < ActiveRecord::Base
   validates :title,  :presence => true
   validates :description, :presence => true,
                     :length => { :minimum => 5 }
+  validates_presence_of :user
+
   has_many :fact_tags
   has_many :tags, :through => :fact_tags
   has_many :comments
