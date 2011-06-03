d75ef4f3d1f2dc2d9de8172c76463f83dd61fb64
Albert Bellonch <albert@itnig.net>
2011-06-03 12:14:22 +0200

    Updated seeds with tags and its union to facts

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,26 @@ User.create! :name => "Sergi", :email => "sergi@itnig.net", :password => "passwo
 User.create! :name => "Dulce", :email => "dulce@itnig.net", :password => "password"
 User.create! :name => "Dani", :email => "dani@itnig.net", :password => "password"
 
+# Some tags
+puts "Adding tags..."
+Tag.delete_all
+Tag.create! :name => "Technology"
+Tag.create! :name => "Ruby"
+Tag.create! :name => "Rails"
+Tag.create! :name => "Design"
+Tag.create! :name => "SEO"
+Tag.create! :name => "Marketing"
+Tag.create! :name => "Others"
+Tag.create! :name => "Cucamonga"
+
 # Some facts
 puts "Adding facts and comments..."
 Fact.delete_all
 10.times.each_with_index do |f,i| # facts
   puts "  Creating fact #{i+1}..."
   f = Fact.create! :title => random_text(2,5,"words"), :description => random_text(2,5,"paragraphs"), :user => User.all.sample
+  puts "    Linking to some tags..."
+  f.tags << Tag.all.sample(1+rand(Tag.all.size))
   (rand(10)+1).times.each_with_index do |c,j| # from 1 to 10 random comments
     puts "    Creating comment #{j+1}..."
     Comment.create! :text => random_text(1,3,"paragraphs"), :user => User.all.sample, :fact => f
