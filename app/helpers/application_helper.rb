902f77aca11e4b738ac51dbe77fb3d1953261e74
Albert Bellonch <albert@itnig.net>
2011-06-08 16:52:53 +0200

    Cleaning up some left 'puts'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,11 +10,9 @@ module ApplicationHelper
    end
 
   def transform_fence_code text
-    puts text.inspect
     open = "<pre><code>\n"
     close = "</code></pre>\n"
     n = 0
-    puts text.inspect
     text = text.gsub(/^```/) { |match|
       n = n+=1 # update counter
       ((n%2==0) ? close : open) # open or close pre+code sequence
