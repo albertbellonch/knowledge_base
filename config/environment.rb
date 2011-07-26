f5622a03aff9884ba748541c9f95403b00e769fc
Albert Bellonch <albert@itnig.net>
2011-07-26 17:26:23 +0200

    Loading APP_CONFIG now

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load application config
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/app.yml")[Rails.env]
+
 # Initialize the rails application
 KnowledgeBase::Application.initialize!
