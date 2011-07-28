fd643480f009161e5e8012b3d206f6e1ffd03277
Albert Bellonch <albert@itnig.net>
2011-07-28 17:52:00 +0200

    Adding admin attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110728104732) do
+ActiveRecord::Schema.define(:version => 20110728154147) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(:version => 20110728104732) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "email",                               :default => "",    :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(:version => 20110728104732) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.boolean  "admin",                               :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
