35910858c72e58218b649e4906954caa271a4053
Albert Bellonch <albert@itnig.net>
2011-07-28 12:59:57 +0200

    Updated seeds for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,19 @@ end
 # Some users
 puts "Adding Itnigers..."
 User.delete_all
-User.create! :name => "Albert", :email => "albert@itnig.net", :password => "password"
-User.create! :name => "Roger", :email => "roger@itnig.net", :password => "password"
 User.create! :name => "Bernat", :email => "bernat@itnig.net", :password => "password"
-User.create! :name => "Sergi", :email => "sergi@itnig.net", :password => "password"
+User.create! :name => "Roger", :email => "roger@itnig.net", :password => "password"
 User.create! :name => "Dulce", :email => "dulce@itnig.net", :password => "password"
+User.create! :name => "Sergi", :email => "sergi@itnig.net", :password => "password"
+User.create! :name => "Albert", :email => "albert@itnig.net", :password => "password"
 User.create! :name => "Dani", :email => "dani@itnig.net", :password => "password"
+
+# Some categories
+puts "Adding categories..."
+Category.delete_all
+Category.create! :name => "Engineering"
+Category.create! :name => "SEO"
+Category.create! :name => "Design"
 
 # Some tags
 puts "Adding tags..."
@@ -31,7 +38,7 @@ puts "Adding facts and comments..."
 Fact.delete_all
 100.times.each_with_index do |f,i| # facts
   puts "  Creating fact #{i+1}..."
-  f = Fact.create! :title => "#{random_text(2,5,"words")} #{rand(1000000)}", :description => random_text(2,5,"paragraphs"), :user => User.all.sample
+  f = Fact.create! :title => "#{random_text(2,5,"words")} #{rand(1000000)}", :description => random_text(2,5,"paragraphs"), :user => User.all.sample, :category => Category.all.sample
   puts "    Linking to some tags..."
   f.tags << Tag.all.sample(1+rand(Tag.all.size))
   (rand(10)+1).times.each_with_index do |c,j| # from 1 to 10 random comments
