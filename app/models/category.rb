a49c1691be69245e94e8bccf19469fd519dbd67c
Albert Bellonch <albert@itnig.net>
2011-08-01 17:51:29 +0200

    Category's name is unique

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,9 @@ class Category < ActiveRecord::Base
   has_many :user_categories
   has_many :users, :through => :users
 
+  validates_presence_of :name
+  validates_uniqueness_of :name
+
   def to_s
     name
   end
