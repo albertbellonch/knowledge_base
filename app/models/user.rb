7f0011277812a2dad199ef5a02b5c70ec2066420
Albert Bellonch <albert@itnig.net>
2011-08-01 17:06:11 +0200

    Added ability to pick a subset of categories to be shown in facts#index

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :category_ids
 
   has_many :facts
   has_many :comments
