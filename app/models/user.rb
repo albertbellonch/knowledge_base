0e98b1582f714c3b3877a987a08eef286bb127be
Albert Bellonch <albert@itnig.net>
2011-08-02 18:49:34 +0200

    Current password is only needed in preferences when changing your own password

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,15 @@ class User < ActiveRecord::Base
     (authentications.empty? || !password.blank?) && super
   end
 
+  def update_with_password(params={})
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if
+      params[:password_confirmation].blank?
+    end
+    update_attributes(params)
+  end
+
   def to_s
     name
   end
